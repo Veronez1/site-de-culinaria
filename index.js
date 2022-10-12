450b2598086c15abf35f4e8802d6cfe6e858eb91
Davi Veronez <87334467+Veronez1@users.noreply.github.com>
2022-10-12 14:02:31 -0300

    Add files via upload
    javascript

---
+++ index.js
@@ -0,0 +1,36 @@
+var titulo = ''
+const ingrediente2 = ''
+const ingrediente3 = ''
+const ingrediente4 = ''
+const ingrediente5 = ''
+const ingrediente6 = ''
+
+const descricao1 = ''
+const descricao2 = ''
+const descricao3 = ''
+const descricao4 = ''
+const descricao5 = ''
+const descricao6 = ''
+
+const img = ''
+
+// Está pegando o ID e colocando na variavel
+document.getElementById("1").innerHTML = titulo
+document.getElementById("2").innerHTML = ingrediente2
+document.getElementById("3").innerHTML = ingrediente3
+document.getElementById("4").innerHTML = ingrediente4
+document.getElementById("5").innerHTML = ingrediente5
+document.getElementById("6").innerHTML = ingrediente6
+
+
+document.getElementById("descricao1").innerHTML = descricao1
+document.getElementById("descricao2").innerHTML = descricao2
+document.getElementById("descricao3").innerHTML = descricao3
+document.getElementById("descricao4").innerHTML = descricao4
+document.getElementById("descricao5").innerHTML = descricao5
+document.getElementById("descricao6").innerHTML = descricao6
+
+
+document.getElementById("img").src = img
+
+img.src = img
